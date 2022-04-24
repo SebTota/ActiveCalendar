3d1ac14a340e9ae822ed01687ca68ebcc24d5453
SebTota <kidokyo1@gmail.com>
2022-04-24 08:31:57 -0700

    Added param check to strava webhook endpoints

--- strava_calendar_summary_web_service/routers/strava_webhook.py
+++ strava_calendar_summary_web_service/routers/strava_webhook.py
@@ -6,9 +6,13 @@ router = APIRouter()
 
 @router.get('/webhook')
 def get_webhook(request: Request):
-    hub_mode = request.query_params['hub.mode']
-    hub_verification_token = request.query_params['hub.verify_token']
-    hub_challenge = request.query_params['hub.challenge']
+    params = request.query_params
+    if 'hub.mode' not in params or 'hub.verify_token' not in params or 'hub.challenge' not in params:
+        raise HTTPException(status_code=400, detail='Missing request parameters')
+
+    hub_mode = params['hub.mode']
+    hub_verification_token = params['hub.verify_token']
+    hub_challenge = params['hub.challenge']
 
     if hub_mode == 'subscribe' and hub_verification_token == STRAVA_WEBHOOK['VERIFY_TOKEN']:
         print('Webhook verified')
@@ -20,10 +24,15 @@ def get_webhook(request: Request):
 @router.post('/webhook')
 def post_webhook(request: Request):
     body = request.json()
+
+    if 'subscription_id' not in body or 'object_type' not in body or 'aspect_type' not in body \
+        or 'owner_id' not in body or 'object_id' not in body:
+        raise HTTPException(status_code=400, detail='Missing request parameters')
+
     subscription_id = body['subscription_id']
 
     if int(subscription_id) != int(STRAVA_WEBHOOK['SUBSCRIPTION_ID']):
-        raise HTTPException(status_code=400, detail='invalid subscription id')
+        raise HTTPException(status_code=400, detail='Invalid subscription id')
 
     object_type = body['object_type']
     aspect_type = body['aspect_type']  ## Create, update, delete
