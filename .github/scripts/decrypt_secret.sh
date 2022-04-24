8d946afce20191dcb6500ed8e4fc9d93a01c27f7
SebTota <kidokyo1@gmail.com>
2022-04-23 22:07:38 -0700

    Created auto deployment to GCP Compute Engine

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,6 @@
+ #!/bin/sh
+    
+# Decrypt the file
+mkdir $GITHUB_WORKSPACE/secrets
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output $GITHUB_WORKSPACE/strava_calendar_summary_web_service/config.py strava_calendar_summary_web_service/config.py.gpg
