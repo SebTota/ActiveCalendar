478a2af0c89268daa9b4d7ef0aa31af5a8e698ae
SebTota <kidokyo1@gmail.com>
2022-05-04 17:55:19 -0700

    Added more logging to Strava webhook router

--- strava_calendar_summary_web_service/routers/strava_webhook.py
+++ strava_calendar_summary_web_service/routers/strava_webhook.py
@@ -12,6 +12,7 @@ router = APIRouter()
 def get_webhook(request: Request):
     params = request.query_params
     if 'hub.mode' not in params or 'hub.verify_token' not in params or 'hub.challenge' not in params:
+        logging.error('Failed registering webhook due to missing request parameters.')
         raise HTTPException(status_code=400, detail='Missing required request parameters')
 
     hub_mode = params['hub.mode']
@@ -19,9 +20,11 @@ def get_webhook(request: Request):
     hub_challenge = params['hub.challenge']
 
     if hub_mode == 'subscribe' and hub_verification_token == os.getenv('STRAVA_WEBHOOK_VERIFICATION_TOKEN'):
-        print('Webhook verified')
+        logging.error('Webhook verified')
         return {'hub.challenge': hub_challenge}
     else:
+        logging.error('Webhook registry request could not be verified due to hub.verify_token being incorrect. Strava '
+                      'hub.verification_token: "{}" with hub.mode: "{}" and the server verificatino code is: "{}"'.format(hub_verification_token, hub_mode, os.getenv('STRAVA_WEBHOOK_VERIFICATION_TOKEN')))
         raise HTTPException(status_code=403, detail='Webhook could not be verified')
 
 
@@ -30,7 +33,7 @@ async def post_webhook(request: Request):
     body = await request.json()
 
     if 'subscription_id' not in body or 'object_type' not in body or 'aspect_type' not in body \
-        or 'owner_id' not in body or 'object_id' not in body or 'event_time' not in body:
+            or 'owner_id' not in body or 'object_id' not in body or 'event_time' not in body:
         raise HTTPException(status_code=400, detail='Missing required request parameters')
 
     subscription_id = body['subscription_id']
@@ -39,28 +42,23 @@ async def post_webhook(request: Request):
         raise HTTPException(status_code=400, detail='Invalid subscription id')
 
     object_type = body['object_type']  # activity, athlete
-    aspect_type = body['aspect_type']  ## create, update, delete
+    aspect_type = body['aspect_type']  # create, update, delete
     user_id = body['owner_id']
     activity_id = body['object_id']
     event_time = body['event_time']
-
-    try:
-        updates = body['updates']
-    except:
-        updates = None
+    updates = body['updates'] if 'updates' in body else {}
 
     new_event: StravaEvent = StravaEvent(object_type, activity_id, aspect_type, updates, user_id, event_time)
-
-    # Ignore none 'activity' events
-    if object_type != 'activity':
-        logging.info('Skipping "{}" activity: {} for user: {}'.format(object_type, activity_id, user_id))
-        return {}
 
     logging.info('Received a new "{}" activity: {} event for user: {}'.format(
         aspect_type, activity_id, user_id))
 
     try:
         message_id = StravaEventMiddleware().publish_strava_event(new_event)
-        logging.info("Published message: {} for strava event id: {} for user: {} to event middleware.".format(message_id, activity_id, user_id))
+        logging.info(
+            "Published message: {} for strava event id: {} for user: {} to event middleware.".format(message_id,
+                                                                                                     activity_id,
+                                                                                                     user_id))
     except Exception as err:
-        logging.error('Failed to publish strava event id: {} for user: {} to the event middleware. Error: {}', activity_id, user_id, err)
+        logging.error('Failed to publish strava event id: {} for user: {} to the event middleware. Error: {}',
+                      activity_id, user_id, err)
