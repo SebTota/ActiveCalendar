f6a59bdee5b6235ba371f9b1002fcb8f7b180160
SebTota <kidokyo1@gmail.com>
2022-05-04 18:55:41 -0700

    Added secret to environ script to Dockerfile

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,3 @@
  #!/bin/sh
-    
-mkdir $GITHUB_WORKSPACE/secrets
-
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output .github/scripts/secrets.sh .github/scripts/secrets.sh.gpg
-
-. ./.github/scripts/secrets.sh
