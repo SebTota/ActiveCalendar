5715a7075f5d845b9d328eeb35ed5ff78f5bf9ce
SebTota <kidokyo1@gmail.com>
2022-05-14 17:54:50 -0700

    Added base path for prod deployment

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/ StravaCalendarSummaryWebService/" : "/",
    configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
