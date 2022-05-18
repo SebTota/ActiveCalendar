320e42b1d5b80e3dd7c6652c56a7581fdf5efe45
SebTota <kidokyo1@gmail.com>
2022-05-18 08:57:53 -0700

    Updated decrypt script for new secretes file names

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,7 +3,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output .github/scripts/secrets.sh .github/scripts/secrets.sh.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output private_key.pem private_key.pem.gpg
+--output key.pem key.pem.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output cert_key.pem cert_key.pem.gpg
+--output cert.pem cert.pem.gpg
