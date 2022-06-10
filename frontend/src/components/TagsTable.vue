8aafb814a7124883007ef2f88ea185976f2f44b8
SebTota <kidokyo1@gmail.com>
2022-06-09 21:45:18 -0700

    Table component for displaying tags and descriptions for template builder

---
+++ frontend/src/components/TagsTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <table class="table table-sm">
+      <div style="height: 400px; overflow: scroll">
+        <thead>
+        <tr>
+          <th scope="col">Tag Name</th>
+          <th scope="col">Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(k, v) in tags" v-bind:key="(k, v)">
+          <th scope="row">{{ v }}</th>
+          <td>{{ k }}</td>
+        </tr>
+        </tbody>
+      </div>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    tags: {}
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+})
+
+</script>
+
+<style scoped>
+
+</style>
