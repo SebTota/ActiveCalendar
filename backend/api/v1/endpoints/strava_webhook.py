5492e79ee5e09b169d54ca1ac8e4c41538092119
Seb Tota <kidokyo1@gmail.com>
2023-04-11 20:31:53 -0700

    Updated logging of strava webhook routes

--- backend/api/v1/endpoints/strava_webhook.py
+++ backend/api/v1/endpoints/strava_webhook.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timezone
 
 from fastapi import APIRouter, Request, HTTPException
-import logging
 
 from backend import schemas
+from backend.core import logger
 from backend.core.config import settings
 
 router = APIRouter()
@@ -13,7 +13,7 @@ router = APIRouter()
 def subscribe_webhook(request: Request):
     params = request.query_params
     if 'hub.mode' not in params or 'hub.verify_token' not in params or 'hub.challenge' not in params:
-        logging.error('Failed registering webhook due to missing request parameters.')
+        logger.error('Failed registering webhook due to missing request parameters.')
         raise HTTPException(status_code=400, detail='Missing required request parameters')
 
     hub_mode = params['hub.mode']
@@ -21,11 +21,11 @@ def subscribe_webhook(request: Request):
     hub_challenge = params['hub.challenge']
 
     if hub_mode == 'subscribe' and hub_verification_token == settings.STRAVA_WEBHOOK_VERIFICATION_TOKEN:
-        logging.info('Webhook verified')
+        logger.info('Webhook verified')
         return {'hub.challenge': hub_challenge}
     else:
-        logging.error('Webhook registry request could not be verified due to hub.verify_token being incorrect. Strava '
-                      'hub.verification_token: "{}" with hub.mode: "{}".'.format(hub_verification_token, hub_mode))
+        logger.error('Webhook registry request could not be verified due to hub.verify_token being incorrect. Strava '
+                     'hub.verification_token: "{}" with hub.mode: "{}".'.format(hub_verification_token, hub_mode))
         raise HTTPException(status_code=403, detail='Webhook could not be verified')
 
 
@@ -35,14 +35,14 @@ async def get_activity_webhook(request: Request):
 
     if 'subscription_id' not in body or 'object_type' not in body or 'aspect_type' not in body \
             or 'owner_id' not in body or 'object_id' not in body or 'event_time' not in body:
-        logging.error('Error processing new event from webhook due to missing parameters in the request body.')
+        logger.error('Error processing new event from webhook due to missing parameters in the request body.')
         raise HTTPException(status_code=400, detail='Missing required request parameters')
 
     subscription_id = body['subscription_id']
 
     if int(subscription_id) != int(settings.STRAVA_SUBSCRIPTION_ID):
-        logging.error('Error processing new event from webhook because the "subscription_id" '
-                      'is different than expected. Provided subscription_id={}'.format(subscription_id))
+        logger.error('Error processing new event from webhook because the "subscription_id" '
+                     'is different than expected. Provided subscription_id={}'.format(subscription_id))
         raise HTTPException(status_code=400, detail='Invalid subscription id')
 
     strava_notification: schemas.StravaNotification = schemas.StravaNotification(
@@ -54,4 +54,4 @@ async def get_activity_webhook(request: Request):
         event_time=datetime.fromtimestamp((int(body['event_time'])), timezone.utc)
     )
 
-    logging.info('Received a new strava activity: {}'.format(strava_notification.dict()))
+    logger.info('Received a new strava activity: {}'.format(strava_notification.dict()))
