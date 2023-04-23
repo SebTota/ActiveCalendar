b1605dab7ab12c6c5d78fd1182fd059dba3fcf26
Seb Tota <kidokyo1@gmail.com>
2023-04-22 19:47:10 -0700

    Removed unused data model

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,3 @@
-from .session_data import SessionData
 from .strava_credentials import StravaCredentials
 from .user import User, UserStatus
 from .google_auth import GoogleAuth
--- backend/models/session_data.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class SessionData(BaseModel):
-    strava_credentials: dict
-    google_auth_state: dict
-    google_credentials: dict
