0b7e15a0bc6457ad652cd2d2896593a75125eef5
Seb Tota <kidokyo1@gmail.com>
2023-04-23 19:28:02 -0700

    Get all templates for a given user

--- backend/crud/crud_calendar_template.py
+++ backend/crud/crud_calendar_template.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from backend.crud.base import CRUDBase
 from backend.models import CalendarTemplate, User
+from backend.models.calendar_template import CalendarTemplateStatus
 from backend.schemas import CalendarTemplateCreate, CalendarTemplateUpdate, CalendarTemplateType
 from backend.utils.base_utils import get_random_alphanumeric_string
 
@@ -13,6 +14,21 @@ class CRUDCalendarTemplate(CRUDBase[CalendarTemplate, CalendarTemplateCreate, Ca
     def get_template_by_type(self, db: Session,
                              user_id: str, template_type: CalendarTemplateType) -> Optional[CalendarTemplate]:
         return db.query(CalendarTemplate).filter(User.id == user_id, CalendarTemplate.type == template_type).first()
+
+    def get_all_active_templates(self, db: Session, user_id: str) -> dict:
+        """
+        Get a dictionary that contains all the active templates for a given user.
+
+        {
+            models.CalendarTemplateType: models.CalendarTemplate
+        }
+        """
+        temps: {} = {}
+        templates = db.query(CalendarTemplate).filter(User.id == user_id,
+                                                      CalendarTemplate.status == CalendarTemplateStatus.ACTIVE).all()
+        for template in templates:
+            temps[template.type] = template
+        return temps
 
     def create_and_add_to_user(self, db: Session, user_id: str, obj: CalendarTemplateUpdate):
         db_obj: CalendarTemplate = CalendarTemplate(id=get_random_alphanumeric_string(12),
