5185520900da42f979755fc80b9c6a2d8a38ac59
Seb Tota <kidokyo1@gmail.com>
2023-04-28 16:52:02 -0700

    Fixed date utils beginning and end of week methods

--- backend/utils/date_utils.py
+++ backend/utils/date_utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone, timedelta
+from backend.core import logger
 
 
 def beginning_of_day_in_utc(date: datetime) -> datetime:
@@ -15,13 +16,15 @@ def beginning_of_week_in_utc(date: datetime) -> datetime:
     """
     Get the date for the beginning of the week of the given date in UTC, assuming Monday is the FIRST day of the week.
     """
-    return end_of_day_in_utc(date) - timedelta(days=7)
+    d = date.replace(hour=0, minute=0, second=0, microsecond=0)
+    d = d - timedelta(days=d.weekday())
+    return datetime.fromtimestamp(d.timestamp(), tz=timezone.utc)
 
 
 def end_of_week_in_utc(date: datetime) -> datetime:
     """
     Get the date for the end of the week of the given date in UTC, assuming Sunday is the LAST day of the week.
     """
-    d_utc = beginning_of_day_in_utc(date)
-    days = ((6 - d_utc.weekday() + 7) % 7) + 1
-    return date + timedelta(days=days)
+    d = date.replace(hour=23, minute=59, second=59, microsecond=0)
+    d = d - timedelta(days=d.weekday()) + timedelta(days=6)
+    return datetime.fromtimestamp(d.timestamp(), tz=timezone.utc)
