44ea64bccf9396817320ecbaeed6c3fa6ceb3d0d
Seb Tota <kidokyo1@gmail.com>
2023-04-28 16:55:29 -0700

    Save summary calendar events for user + other cleanup

--- backend/background_tasks/workers/strava_notification_background_task_worker.py
+++ backend/background_tasks/workers/strava_notification_background_task_worker.py
@@ -7,11 +7,12 @@ from backend import schemas, crud, models
 from backend.accessors import StravaAccessor, GoogleCalendarAccessor
 from backend.core import logger
 from backend.db.session import SessionLocal
-from backend.models import StravaCredentials
+from backend.models import StravaCredentials, CalendarEvent
 from backend.models.calendar_template import CalendarTemplateType
-from backend.schemas import CalendarEventCreate, StravaNotificationAction, CalendarEventUpdate
+from backend.schemas import CalendarEventCreate, StravaNotificationAction
 from backend.utils import calendar_template_utils
-from backend.utils.date_utils import beginning_of_day_in_utc, end_of_day_in_utc
+from backend.utils.date_utils import beginning_of_day_in_utc, end_of_day_in_utc, beginning_of_week_in_utc, \
+    end_of_week_in_utc
 
 
 class StravaNotificationBackgroundTaskWorker:
@@ -47,7 +48,7 @@ class StravaNotificationBackgroundTaskWorker:
                                                                                                self._user_id)
         if CalendarTemplateType.ACTIVITY_SUMMARY in templates.keys():
             template: models.CalendarTemplate = templates.pop(CalendarTemplateType.ACTIVITY_SUMMARY)
-            self._process_activity_template(activity, template)
+            # self._process_activity_template(activity, template)
 
         # There is nothing to change on an activity update for the summary templates, so we will take
         #  no actions on the summary events on update.
@@ -91,7 +92,8 @@ class StravaNotificationBackgroundTaskWorker:
             cal_event_create: CalendarEventCreate = CalendarEventCreate(calendar_event_id=cal_event_id,
                                                                         strava_event_id=activity.id,
                                                                         start_date=activity.start_date,
-                                                                        end_date=(activity.start_date + activity.moving_time))
+                                                                        end_date=(
+                                                                                    activity.start_date + activity.moving_time))
 
             crud.calendar_event.create_and_add_to_user(db=self._db, user_id=self._user_id, obj_create=cal_event_create)
             logger.debug(f"Created new activity calendar event: {cal_event_id} "
@@ -105,22 +107,44 @@ class StravaNotificationBackgroundTaskWorker:
             end_date: datetime = end_of_day_in_utc(activity.start_date_local)
             activities = self._strava_accessor.get_activities(start_date, end_date)
         elif summary_type == CalendarTemplateType.WEEKLY_SUMMARY:
-            start_date: datetime = beginning_of_day_in_utc(activity.start_date_local)
-            end_date: datetime = end_of_day_in_utc(activity.start_date_local)
+            start_date: datetime = beginning_of_week_in_utc(activity.start_date_local)
+            end_date: datetime = end_of_week_in_utc(activity.start_date_local)
             activities = self._strava_accessor.get_activities(start_date, end_date)
         else:
             raise Exception(f"Failed to process summary template for user: {self._user_id} due to "
                             f"invalid summary type: {summary_type}")
 
-        gen_title_template: str = calendar_template_utils.fill_summary_template(activity_template.title_template, activities)
-        gen_body_template: str = calendar_template_utils.fill_summary_template(activity_template.body_template, activities)
+        logger.debug(f"Generating summary templates for user: {self._user_id} between {start_date} and {end_date}")
+        gen_title_template: str = calendar_template_utils.fill_summary_template(activity_template.title_template,
+                                                                                activities)
+        gen_body_template: str = calendar_template_utils.fill_summary_template(activity_template.body_template,
+                                                                               activities)
 
-        cal_event_id: str = self._cal_accessor.add_event(gen_title_template,
-                                                         gen_body_template,
-                                                         str(activity.timezone),
-                                                         start_date,
-                                                         end_date,
-                                                         True)
+        existing_cal_event: Optional[CalendarEvent] = crud.calendar_event.get_by_start_and_end_date(self._db,
+                                                                                                    self._user_id,
+                                                                                                    start_date,
+                                                                                                    end_date)
 
-        logger.debug(f"Created new summary calendar event: {cal_event_id} "
-                     f"for user: {self._user_id} and Strava activity: {activity}.")
+        if existing_cal_event is None:
+            cal_event_id: str = self._cal_accessor.add_event(gen_title_template,
+                                                             gen_body_template,
+                                                             str(activity.timezone),
+                                                             start_date,
+                                                             end_date,
+                                                             True)
+
+            cal_event_create: CalendarEventCreate = CalendarEventCreate(calendar_event_id=cal_event_id,
+                                                                        start_date=start_date,
+                                                                        end_date=end_date)
+            crud.calendar_event.create_and_add_to_user(db=self._db, user_id=self._user_id, obj_create=cal_event_create)
+            logger.debug(f"Created new summary calendar event: {cal_event_id} "
+                         f"for user: {self._user_id} and Strava activity: {activity}.")
+        else:
+            logger.debug(f"Updating event: {existing_cal_event} for user: {self._user_id}.")
+            self._cal_accessor.update_event(existing_cal_event.calendar_event_id,
+                                            gen_title_template,
+                                            gen_body_template,
+                                            str(activity.timezone),
+                                            start_date,
+                                            end_date,
+                                            True)
--- backend/crud/crud_calendar_event.py
+++ backend/crud/crud_calendar_event.py
@@ -4,7 +4,7 @@ from typing import Optional
 from sqlalchemy.orm import Session
 
 from backend.crud.base import CRUDBase
-from backend.models import CalendarEvent
+from backend.models import CalendarEvent, User
 from backend.schemas import CalendarEventCreate, CalendarEventUpdate
 from backend.utils.base_utils import get_random_alphanumeric_string
 
@@ -12,9 +12,10 @@ class CRUDCalendarEvent(CRUDBase[CalendarEvent, CalendarEventCreate, CalendarEve
     def get_by_strava_event_id(self, db: Session, strava_event_id: int) -> Optional[CalendarEvent]:
         return db.query(CalendarEvent).filter(CalendarEvent.strava_event_id == strava_event_id).first()
 
-    def get_by_start_and_end_date(self, db: Session, start_date: datetime, end_date: datetime) -> [CalendarEvent]:
-        return db.query(CalendarEvent).filter(CalendarEvent.start_date == start_date,
-                                              CalendarEvent.end_date == end_date).all()
+    def get_by_start_and_end_date(self, db: Session, user_id: str, start_date: datetime, end_date: datetime) -> Optional[CalendarEvent]:
+        return db.query(CalendarEvent).filter(User.id == user_id,
+                                              CalendarEvent.start_date == start_date,
+                                              CalendarEvent.end_date == end_date).first()
 
     def create_and_add_to_user(self, db: Session, user_id: str, obj_create: CalendarEventCreate):
         new_obj: CalendarEvent = CalendarEvent(id = get_random_alphanumeric_string(20),
