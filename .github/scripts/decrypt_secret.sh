6af82d152730eb0ff0e4b5d363b909ad9a8195b6
Seb Tota <kidokyo1@gmail.com>
2023-04-28 18:26:53 -0700

    Update GitHub actions secrets decrypt script.

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,4 +1,3 @@
- #!/bin/sh
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output nginx/bundle.pem nginx/bundle.pem.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output client_secret.json client_secret.json.gpg
+#!/bin/sh
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output .env .env.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output backend/client_secret.json backend/client_secret.json.gpg
