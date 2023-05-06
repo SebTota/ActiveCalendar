7a9293561969cd33811295b4901b6d100c6cda2c
Seb Tota <kidokyo1@gmail.com>
2023-05-05 18:56:48 -0700

    Fixing crud import statements.

--- backend/crud/__init__.py
+++ backend/crud/__init__.py
@@ -1,6 +1,5 @@
 from backend.crud import crud_calendar_event as calendar_event
-
-# import crud_google_calendar_auth as google_calendar_auth
-# import crud_user as user
-# import crud_strava_credentials as strava_credentials
-# import crud_calendar_template as calendar_template
+from backend.crud import crud_google_calendar_auth as google_calendar_auth
+from backend.crud import crud_user as user
+from backend.crud import crud_calendar_template as calendar_template
+from backend.crud import crud_strava_credentials as strava_credentials
