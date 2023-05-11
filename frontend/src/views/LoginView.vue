4598b6f4c99e3f57e3f96ac6ba13f271a7ac241d
Seb <kidokyo1@gmail.com>
2023-05-11 07:55:17 -0700

    Refactoring improper HTML tags in login view.

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,16 +1,16 @@
 <template>
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <img className="mx-auto h-50 w-auto" src="../assets/ActiveRunWideLogo.png" alt="Active" />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-300">Sign in or create
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <img class="mx-auto h-50 w-auto" src="../assets/ActiveRunWideLogo.png" alt="Active" />
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-300">Sign in or create
                 your
                 account.</h2>
-            <p className="text-center text-l font-bold leading-9 tracking-tight text-gray-300">Sign in with your Google
+            <p class="text-center text-l font-bold leading-9 tracking-tight text-gray-300">Sign in with your Google
                 account below.</p>
         </div>
     </div>
 
-    <div className="text-center">
+    <div class="text-center">
         <template v-if="showLoading">
             <div class="flex justify-center items-center">
                 <div class="flex items-center justify-center">
@@ -20,11 +20,11 @@
                     </div>
                 </div>
             </div>
-            <h2 className="text-center text-l font-bold leading-9 tracking-tight text-gray-300">Signing In...</h2>
+            <h2 class="text-center text-l font-bold leading-9 tracking-tight text-gray-300">Signing In...</h2>
         </template>
         <template v-else>
             <a :href="googleAuthPath">
-                <img src="../assets/google_sign_in.png" className="inline" />
+                <img src="../assets/google_sign_in.png" class="inline" alt="Google Sign In Button" />
             </a>
         </template>
     </div>
