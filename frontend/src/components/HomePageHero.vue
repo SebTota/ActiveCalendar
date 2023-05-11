824424faa54422a9c3f0c90fba18be5d27b14764
Seb <kidokyo1@gmail.com>
2023-05-11 08:25:57 -0700

    Fixed class name declarations in home page hero component

--- frontend/src/components/HomePageHero.vue
+++ frontend/src/components/HomePageHero.vue
@@ -3,17 +3,17 @@
 </script>
 
 <template>
-  <div className="hero min-h-screen bg-gray-800">
-    <div className="hero-content flex-col lg:flex-row-reverse bg-gray-800">
-      <div className="bg-gray-800">
-        <img src="/src/assets/mock_one.png" className="max-w-sm rounded-lg shadow-2xl" />
+  <div class="hero min-h-screen bg-gray-800">
+    <div class="hero-content flex-col lg:flex-row-reverse bg-gray-800">
+      <div class="bg-gray-800">
+        <img src="/src/assets/mock_one.png" alt="Phone mock with calendar view" class="max-w-sm rounded-lg shadow-2xl" />
       </div>
       <div>
-        <h1 className="text-5xl font-bold">Active Calendar</h1>
-        <p className="py-6">Track all your activities, right in your Google Calendar. Get per activity, daily, and weekly
+        <h1 class="text-5xl font-bold">Active Calendar</h1>
+        <p class="py-6">Track all your activities, right in your Google Calendar. Get per activity, daily, and weekly
           activity summaries to track your total mileage.
         </p>
-        <button className="btn btn-primary">Get Started</button>
+        <button class="btn btn-primary">Get Started</button>
       </div>
     </div>
   </div>
