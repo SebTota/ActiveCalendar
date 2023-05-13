7b84495848e43a72a8cdd7d9dfc820dc14e668f9
Seb <kidokyo1@gmail.com>
2023-05-12 20:27:56 -0700

    Set main app area height to remaining after header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,19 @@
 <template>
     <router-view :key="this.$route.path">
-          <Header />
+        <Header/>
     </router-view>
-  <RouterView />
+    <div class="remaining_height">
+        <RouterView/>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
 import Header from './components/Header.vue'
 </script>
+
+<style scoped>
+    .remaining_height {
+        height: Calc(100% - 64px - 2px);
+    }
+</style>
