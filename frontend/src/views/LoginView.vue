d4708d944d2536dca2a18b2d095242db191d81d9
Seb <kidokyo1@gmail.com>
2023-05-12 20:27:56 -0700

    Set main app area height to remaining after header

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <img class="mx-auto h-50 w-auto" src="../assets/ActiveRunWideLogo.png" alt="Active" />
             <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-300">Sign in or create
