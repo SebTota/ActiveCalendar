ce316e56330484f5ab7d9a1828a7e11f9b5b8605
Seb <kidokyo1@gmail.com>
2023-05-12 20:27:56 -0700

    Set main app area height to remaining after header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <Disclosure as="nav" class="border-b-2 border-gray-400" v-slot="{ open }">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
