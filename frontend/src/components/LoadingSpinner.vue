400597529847602eff1c9480cec88f2c445d4569
Seb <kidokyo1@gmail.com>
2023-05-25 17:22:48 -0700

    Changed background base color

--- frontend/src/components/LoadingSpinner.vue
+++ frontend/src/components/LoadingSpinner.vue
@@ -4,7 +4,7 @@
             <div class="flex items-center justify-center">
                 <div
                         class="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-tr from-white to-gray-600 animate-spin">
-                    <div class="h-9 w-9 rounded-full bg-gray-700"></div>
+                    <div class="h-9 w-9 rounded-full bg-zinc-900"></div>
                 </div>
             </div>
         </div>
