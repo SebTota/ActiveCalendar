d33f7187648c5b027b601286cd2fabbe90c88428
Seb <kidokyo1@gmail.com>
2023-05-27 10:32:22 -0700

    Update homepage to simple hero.

--- frontend/src/components/HomePageHero.vue
+++ frontend/src/components/HomePageHero.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="hero h-full">
-    <div class="hero-content h-full flex-col lg:flex-row-reverse">
-      <div>
-        <img src="/src/assets/mock_one.png" alt="Phone mock with calendar view" class="max-w-100 hidden lg:block sm:max-w-sm rounded-lg shadow-2xl" />
-      </div>
-      <div>
-        <h1 class="text-5xl font-bold">Active Calendar</h1>
-        <p class="py-6">Track all your activities, right in your Google Calendar. Get per activity, daily, and weekly
-          activity summaries to track your total mileage.
-        </p>
-        <button class="btn btn-primary" @click="navigateGetStarted">Get Started</button>
-      </div>
+    <div class="h-full px-2 sm:px-6">
+
+        <div class="container mx-auto flex px-2 py-24 md:flex-row flex-col items-center max-w-7xl text-gray-300">
+            <div class="items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-left">Active Calendar</h1>
+                <p class="mb-8 leading-relaxed">Track all your activities, right in your Google Calendar. Get per activity, daily, and weekly
+                    activity summaries to track your total mileage.</p>
+                <div class="flex">
+                    <button @click="navigateGetStarted" class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                        Get Started
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -21,7 +22,7 @@ import {useMainStore} from "@/stores/state";
 const mainStore = useMainStore();
 
 function navigateGetStarted() {
-    if(mainStore.isLoggedIn) {
+    if (mainStore.isLoggedIn) {
         mainStore.redirectToSettings();
     } else {
         mainStore.redirectToLogin();
